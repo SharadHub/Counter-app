6037bdc8ff4187afa5ed65a862df2be08658c08e
Sharad Bista <bishalbista737@gmail.com>
2025-12-24 14:49:59 +0545

    deleted trash, added css

---
+++ React projects/Counter-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Counter from "./components/counter";
+
+function App() {
+  return (
+    <>
+      <h1>React Counter</h1>
+      <hr />
+      <Counter/>
+      <hr/>
+    </>
+  );
+}
+
+export default App
---
+++ React projects/Counter-app/src/components/counter.jsx
@@ -0,0 +1,28 @@
+import "./counter.css"
+import { useState } from 'react';
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+
+    const decrement = () => {
+        setCount(count - 1);
+    }
+
+    const reset = () => {
+        setCount(0);
+    }
+
+    return (
+        <div className="counter">
+            <div className="count">{count}</div>
+            <button className="decrement" onClick={decrement}>-</button>
+            <button onClick={reset}>Reset</button>
+            <button className="increment" onClick={increment}>+</button>
+        </div>
+    );
+}
+export default Counter;
